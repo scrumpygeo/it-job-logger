2b327bb7a2241d346743550e5a2b9b685a872297
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-28 12:15:04 +0000

    add Searchbar and Logs components

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+const Logs = () => {
+  // create our state
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+    // eslint-disable-next-line
+  }, []); // pass [] so it just runs once
+
+  const getLogs = async () => {
+    setLoading(true);
+
+    const res = await fetch('/logs');
+    const data = await res.json(); // formats and gives us the data
+
+    setLogs(data); // the action.
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <h4>Loading...</h4>;
+  }
+
+  return (
+    <ul className='collection-with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
+      </li>{' '}
+      {!loading && logs.length === 0 ? (
+        <p className='center'>No logs to show...</p>
+      ) : (
+        logs.map(log => <li>{log.message}</li>)
+      )}
+    </ul>
+  );
+};
+
+export default Logs;
