c2ba821bdd3e54802f4f6b9fb111889d52578894
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-03-02 13:57:23 +0000

    added edit and search logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,11 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS,
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from './types';
 
 // more bulky way: below is refactored + try catch
@@ -79,6 +83,68 @@ export const deleteLog = id => async dispatch => {
     dispatch({
       type: DELETE_LOG,
       payload: id
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+// Search server logs action
+export const searchLogs = text => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+// Set current log action
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  };
+};
+
+// Clear Current Log action
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  };
+};
+
+// Update log from server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
     });
   } catch (err) {
     dispatch({
