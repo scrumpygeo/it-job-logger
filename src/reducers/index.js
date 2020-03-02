efa44e767a8429e56a9a33961ecabc3f29907219
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-03-02 15:37:59 +0000

    set up techs with actions and reducer basics

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import logReducer from './logReducer';
+import techReducer from './techReducer';
 
 export default combineReducers({
-  log: logReducer // log is what we are calling our state
+  log: logReducer, // log is what we are calling our state
+  tech: techReducer
 });
